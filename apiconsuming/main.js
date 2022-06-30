f0a8890240c316bfef4f40025d7eccc81e9e58fd
Marcelo Leiva <marcelo.leiva1@virginiogomez.cl>
2022-06-30 18:31:28 -0400

    m

--- apiconsuming/main.js
+++
@@ -1,9 +0,0 @@
-API()
-function API()
-{
-    fetch('https://sereapi.azurewebsites.net/api/Countries',)
-    .then(response => response.txt())
-    .then(data => console.log(data))
-}
-
-console.log(data);
