3e27cd00b7ee2247e338efb2613024c55a1f3722
Marcelo Leiva <marcelo.leiva1@virginiogomez.cl>
2022-07-14 18:03:17 -0400

    fetch

--- full/lp2/Index2/forms/register.js
+++ full/lp2/Index2/forms/register.js
@@ -1,22 +1,25 @@
-const button = document.getElementById("button");
+const button = document.getElementById('button');
 
-button.addEventListener("click", () => {
-  const newPost = {
-    nombre: document.getElementById("names").value,
-    apellido: document.getElementById("apellidos").value,
-    correo: document.getElementById("emailAddress").value,
-    numero: document.getElementById("phoneNumber").value,
-    pass: document.getElementById("pass").value,
-    organizacion: document.getElementById("orgName").value,
-    nacionalidad: 1
-  };
-  fetch('https://localhost:7146/api/People',  {
-    method: "POST",
-    body: JSON.stringify(newPost),
-    headers: {
-      "access-control-allow-origin": "*", 
-      "Content-type": "text/plain; charset=utf-8",
-      "server": "Kestrel"
-    },
-  }).then(res => res.json())
+button.addEventListener('click', () => {
+  // const newPost = {
+  //   nombre: document.getElementById('names').value,
+  //   apellido: document.getElementById('apellidos').value,
+  //   pass: document.getElementById('emailAddress').value,
+  //   numero: document.getElementById('phoneNumber').value,
+  //   pass: document.getElementById('pass').value,
+  //   nacionalidad: 1,
+  // };
+  const newPost =  `?nombre=${document.getElementById('names').value.trim()}
+  &apellido=${document.getElementById('apellidos').value.trim()}
+  &correo=${document.getElementById('emailAddress').value.trim()}
+  &numero=${document.getElementById('phoneNumber').value.trim()}
+  &pass=${document.getElementById('pass').value.trim()}
+  &organizacion=${document.getElementById('orgName').value.trim()}
+  &nacionalidad=1
+  `;
+  fetch('https://localhost:7146/api/People' + newPost, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    //body: JSON.stringify(newPost),
+  }).then((res) => res.json());
 });
