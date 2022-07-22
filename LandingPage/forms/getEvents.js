98cd9da73f8cb0f377cd217d9dce3f71f52fbccb
Patogallina <michael.escobar@virginiogomez.cl>
2022-07-22 01:52:16 -0400

    Elleiva

--- LandingPage/forms/getEvents.js
+++ LandingPage/forms/getEvents.js
@@ -1,23 +1,39 @@
 
-let data = `?titulo=${document.getElementById("TituloEvento").value}
-    &descripcion=${document.getElementById("descrEvento").value}
-    &fecha=${document.getElementById("FechaEvento").value}
-    &imagen=${document.getElementById("imagenEvento").value}
-    `;
-    const contenedor = document.getElementById('cualquiercosa')
+// let data = `?titulo=${document.getElementById("TituloEvento").value}
+//     &descripcion=${document.getElementById("descrEvento").value}
+//     &fecha=${document.getElementById("FechaEvento").value}
+//     &imagen=${document.getElementById("imagenEvento").value}
+//     `;
+//     const contenedor = document.getElementById('cualquiercosa')
 
-fetch('https://localhost:7146/api/Events'+ data ,{
-    method : "get",
-    renderData : (data) => data.forEach(element => {
-        contenedor.innerHTML += `<div class="card" style="width: 18rem;">
-    <img src="${element.imagen}" class="card-img-top" id="img">
-    <div class="card-body">
-      <h3 href="#">${element.titulo}</h3>
-      <p class="card-text">${element.descripcion}</p>
-      <p>Ubicacion</p>
-      <p>${element.fecha}</p>
-    </div>
-  </div>`
-    })
-    ,renderData(data)
-});
+// fetch('https://localhost:7146/api/Events'+ data ,{
+//     method : "get",
+//     renderData : (data) => data.forEach(element => {
+//         contenedor.innerHTML += `<div class="card" style="width: 18rem;">
+//     <img src="${element.imagen}" class="card-img-top" id="img">
+//     <div class="card-body">
+//       <h3 href="#">${element.titulo}</h3>
+//       <p class="card-text">${element.descripcion}</p>
+//       <p>Ubicacion</p>
+//       <p>${element.fecha}</p>
+//     </div>
+//   </div>`
+//     })
+//     ,renderData(data)
+// });
+
+let API = "https://localhost:7146/api/Events";
+fetch(API)
+  .then(response => response.text())
+  .then(data => showData(data))
+  .catch(error => console.log(error));
+
+
+
+const showData = (data) => {
+  console.log(JSON.parse(data))
+  let body = "";
+  body = `<p>${data.name}</p><p>${data.description}</p><p>${data.date}</p>`
+
+  document.getElementById('data').innerHTML = body
+};
