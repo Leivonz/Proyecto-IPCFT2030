7519c5e5da536f0970404ea2b3a1a52f011f1959
Marcelo Leiva <marcelo.leiva1@virginiogomez.cl>
2022-07-22 01:54:04 -0400

    m

--- LandingPage/forms/getEvents.js
+++ LandingPage/forms/getEvents.js
@@ -1,23 +1,21 @@
+// let data = `?titulo=${document.getElementById("TituloEvento").value}
+//     &descripcion=${document.getElementById("descrEvento").value}
+//     &fecha=${document.getElementById("FechaEvento").value}
+//     &imagen=${document.getElementById("imagenEvento").value}
+//     `;
+//     const contenedor = document.getElementById('cualquiercosa')
+let API = "https://localhost:7146/api/Events";
+fetch(API)
+  .then((response) => response.json)
+  .then((data) => showData(data))
+  .catch((error) => console.log(error));
 
-let data = `?titulo=${document.getElementById("TituloEvento").value}
-    &descripcion=${document.getElementById("descrEvento").value}
-    &fecha=${document.getElementById("FechaEvento").value}
-    &imagen=${document.getElementById("imagenEvento").value}
-    `;
-    const contenedor = document.getElementById('cualquiercosa')
+const showData = (data) => {
+  console.log(data);
+  let body = "";
+  for (let i = 0; i < data.length; i++) {
+    body += `<p></<p>${data[i].name}<p></<p>${data[i].description}<p></<p>${data[i].date}`
+  }
 
-fetch('https://localhost:7146/api/Events'+ data ,{
-    method : "get",
-    renderData : (data) => data.forEach(element => {
-        contenedor.innerHTML += `<div class="card" style="width: 18rem;">
-    <img src="${element.imagen}" class="card-img-top" id="img">
-    <div class="card-body">
-      <h3 href="#">${element.titulo}</h3>
-      <p class="card-text">${element.descripcion}</p>
-      <p>Ubicacion</p>
-      <p>${element.fecha}</p>
-    </div>
-  </div>`
-    })
-    ,renderData(data)
-});
+  document.getElementById('data').innerHTML = body
+};
